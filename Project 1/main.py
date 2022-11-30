6992cfc41abc9e3abefd6dc135bb439c0a8b78e0
UNOSko <119525812+UNOSko@users.noreply.github.com>
2022-11-30 11:22:06 -0600

    Add files via upload

---
+++ Project 1/main.py
@@ -0,0 +1,11 @@
+import sys
+from tvRemoteUI import Television
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    tv1 = Television()
+    tv1.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
